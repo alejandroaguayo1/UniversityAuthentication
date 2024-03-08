3c5a1effc0fd2e7cdb162996fd0c2499fec4915f
alejandroaguayo1 <alejandro77tcm@gmail.com>
2024-03-08 17:18:16 +0100

    Comprobaciones Instructor
    //Esta en la tabla de usuarios
    //Comprobamos si esta en la tabla de Instructores
    //Existe este instructor

--- UniversityAuthentication/Controllers/InstructorController.cs
+++ UniversityAuthentication/Controllers/InstructorController.cs
@@ -1,13 +1,35 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityAuthentication.Data;
 
 namespace UniversityAuthentication.Controllers
 {
     [Authorize(Roles ="Instructor, Admin")]
     public class InstructorController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        public InstructorController(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Registrado"] = false;
+
+            if(User.Identity.Name != null)
+            {
+                //Esta en la tabla de usuarios
+                //Comprobamos si esta en la tabla de Instructores
+                var instructor = await _db.Instructors.FirstOrDefaultAsync(i=>i.InstructorUser == User.Identity.Name);
+                if(instructor != null)
+                {
+                    //Existe este instructor
+                    ViewBag.InstructorId = instructor.InstructorId;
+                    ViewData["Registrado"] = true;
+                }
+            }
+
             return View();
         }
     }
