75a48580525dbd5866daabe8a0786e5056184a16
alejandroaguayo1 <alejandro77tcm@gmail.com>
2024-03-08 16:44:12 +0100

    Creacion de DbContext y gestion de roles
    Creacion de DbContext y gestion de roles en el program cs

--- UniversityAuthentication/Data/ApplicationDbContext.cs
+++ UniversityAuthentication/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UniversityAuthentication.Models;
 
 namespace UniversityAuthentication.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
